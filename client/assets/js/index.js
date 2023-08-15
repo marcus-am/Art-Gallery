9df9deba5c9260e442ab6ce44dec556aacae53b7
Valentin Abrutin <abrutinvalic@gmail.com>
2023-08-15 11:10:37 +0100

    set up frontend structure and style

---
+++ client/assets/js/index.js
@@ -0,0 +1,7 @@
+document.querySelector('#login').addEventListener("click", () => {
+    window.location.assign("login.html");
+});
+
+document.querySelector('#signup').addEventListener("click", () => {
+    window.location.assign("register.html");
+});
