48422515097c2418714c1c18c517ee2d61effb96
marcus-am <marcus_mm@outlook.com>
2023-08-15 11:23:44 +0100

    added sql for users

---
+++ api/database/setup.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS user_account;
+
+CREATE TABLE user_account (
+    user_id INT GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    password CHAR(60) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+INSERT INTO user_account
+    (username, password)
+    VALUES
+    ('Marcus', 'password')
